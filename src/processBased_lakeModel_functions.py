9434462894afcbaef945da45fe08af90f9930bac
emarchisin <marchisin@wisc.edu>
2026-01-13 15:58:07 -0600

    param call fix

--- src/processBased_lakeModel_functions.py
+++ src/processBased_lakeModel_functions.py
@@ -4142,6 +4142,7 @@ def run_wq_model(
         dt = dt,
         dx = dx,
         theta_r = theta_r,
+        theta_npp=theta_npp,
         k_half = k_half,
         resp_docr = resp_docr,
         resp_docl = resp_docl,
